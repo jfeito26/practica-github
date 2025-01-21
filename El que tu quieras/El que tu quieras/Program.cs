84a44978e8b0b6e5a75529f65ff5b26fe47040b4
jfeito26 <jorgefeicar26@gmail.com>
2025-01-21 19:47:52 +0100

    he cambiado el hello world
    he creado un archivo de visual studio 2022 y en vez de hello world he puesto hola dieguete

---
+++ El que tu quieras/El que tu quieras/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace El_que_tu_quieras
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("hola Dieguete");
+        }
+    }
+}
